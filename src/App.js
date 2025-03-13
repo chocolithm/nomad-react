d3d4ed5ca884a4e6b90dc1d5357112b1c2ded309
chocolithm <kys8336@gmail.com>
2025-03-14 01:50:02 +0900

    useEffect 학습

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+  useEffect(() => {
+    console.log("I run when keyword changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when counter changes");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when keyword and counter changes");
+  }, [keyword, counter]);
+
+  return (
+    <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
+}
+
+export default App;
